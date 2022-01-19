9303b83959f79f5cf1dbe6765405b188df4d4544
Stiv Bajro <stiv.bajro@gmail.com>
2022-01-19 12:51:49 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -2,30 +2,33 @@
 
 /* Hamburger Menu Bar */
 function initHamburger() {
-    const hamburger = document.getElementById("hamburger");
-    const menu = document.getElementById("overlay");
-    const js_content_blur = document.getElementById("js_content_blur");
-    let open = false;
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("overlay");
+  const js_content_blur = document.getElementById("js_content_blur");
+  let open = false;
 
-    function change() {
-      if (!open) {
-        hamburger.classList.add("open");
-        menu.classList.add("menu");
-        js_content_blur.style.filter = "blur(6px)";
-        document.body.style.overflow = "hidden";
-      } else {
-        hamburger.classList.remove("open");
-        menu.classList.remove("menu");
-        js_content_blur.style.filter = "none";
-        document.body.style.overflow = "auto";
-      }
-      open = !open;
+  function change() {
+    if (!open) {
+      hamburger.classList.add("open");
+      menu.classList.add("menu");
+      js_content_blur.style.filter = "blur(6px)";
+      document.body.style.overflow = "auto";
+    } else {
+      hamburger.classList.remove("open");
+      overlay.classList.add("hidden");
+      menu.classList.remove("menu");
+      js_content_blur.style.filter = "none";
+      document.body.style.overflow = "auto";
     }
+    open = !open;
+  }
 
-    hamburger.addEventListener("click", change);
+  hamburger.addEventListener("click", change);
+  menu.addEventListener("click", change);
 };
 
 initHamburger();
+
 
 /* h1 animation  */
 $('h1 ').hide().slideDown(1000);
